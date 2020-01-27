a1176fc2b9995f34a8e4c5be00f8f138b131bd69
SkinnyBonesArv <SkinnyBonesArv@Gmail.com>
2020-01-27 13:42:34 -0500

    Add encoder in Shooter

--- src/main/java/com/team175/robot/subsystems/Shooter.java
+++ src/main/java/com/team175/robot/subsystems/Shooter.java
@@ -1,13 +1,17 @@
 package com.team175.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.team175.robot.models.Gains;
 
+// TODO: Add support for moving turret to cardinals as suggested by Boyer.
 public final class Shooter extends SubsystemBase {
 
     private final TalonSRX turret;
 
     private static final int TURRET_PORT = 5;
+    private static final Gains TURRET_GAINS = new Gains(0, 0, 0, 0, 0, 0);
 
     private static Shooter instance;
 
@@ -28,6 +32,13 @@ public final class Shooter extends SubsystemBase {
     private void configureTalons() {
         turret.configFactoryDefault();
         turret.setInverted(true);
+        turret.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        turret.config_kP(0, TURRET_GAINS.getKp());
+        turret.config_kI(0, TURRET_GAINS.getKi());
+        turret.config_kD(0, TURRET_GAINS.getKd());
+        turret.config_kF(0, TURRET_GAINS.getKf());
+        turret.configMotionAcceleration(TURRET_GAINS.getAcceleration());
+        turret.configMotionCruiseVelocity(TURRET_GAINS.getCruiseVelocity());
     }
 
     public void setTurretOpenLoop(double demand) {
