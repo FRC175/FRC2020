cff9f543845147daa10d9f15bbf27ce105dd061d
SkinnyBonesArv <SkinnyBonesArv@Gmail.com>
2020-02-08 20:19:29 -0500

    Add LED subsystem and blink command

---
+++ src/main/java/com/team175/robot/subsystems/LED.java
@@ -0,0 +1,57 @@
+package com.team175.robot.subsystems;
+
+import com.ctre.phoenix.CANifier;
+import edu.wpi.first.wpilibj.util.Color;
+
+public final class LED extends SubsystemBase {
+
+    private final CANifier controller;
+
+    private Color colorSetpoint;
+
+    private static final int CANIFIER_PORT = 0;
+
+    private static LED instance;
+
+    private LED() {
+        controller = new CANifier(CANIFIER_PORT);
+        configCANifier();
+    }
+
+    public static LED getInstance() {
+        if (instance == null) {
+            instance = new LED();
+        }
+
+        return instance;
+    }
+
+    private void configCANifier() {
+        controller.configFactoryDefault();
+    }
+
+    public void setColor(Color color) {
+        colorSetpoint = color;
+        controller.setLEDOutput(color.red, CANifier.LEDChannel.LEDChannelA);
+        controller.setLEDOutput(color.green, CANifier.LEDChannel.LEDChannelB);
+        controller.setLEDOutput(color.blue, CANifier.LEDChannel.LEDChannelC);
+    }
+
+    public void blinkColor(Color color) {
+    }
+
+    public Color getColor() {
+        return colorSetpoint;
+    }
+
+    @Override
+    public void resetSensors() {
+        setColor(Color.kBlack);
+    }
+
+    @Override
+    public boolean checkIntegrity() {
+        return false;
+    }
+
+}
